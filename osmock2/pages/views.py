80f980381588efca443216c875179652c479dab6
WaspVentMan <66962696+WaspVentMan@users.noreply.github.com>
2023-10-31 09:52:51 +0000

    init

---
+++ osmock2/pages/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+	template_name = "index.html"
