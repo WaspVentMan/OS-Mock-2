2f2224897ab13b7cae8dfc018273283d9cf30c56
WaspVentMan <66962696+WaspVentMan@users.noreply.github.com>
2023-11-02 16:52:14 +0000

    marquee deluxe

--- osmock2/pages/static/js/script.js
+++ osmock2/pages/static/js/script.js
@@ -1,12 +1,9 @@
 const PHiL = "https://waspventman.co.uk"
 
-const bannertext = document.querySelectorAll(".bannertext")
+const bannertext = document.querySelector(".bannertext")
 const bannerlist = ["Health Advice Group provides weather info and health advice!", "Hello, World!", "Wow, real boring for a test text, Jacob.", "It's looking wild outside today.", "Yeah! do... that..."]
 
-for (let y = 0; y < bannertext.length; y++){
-    bannertext[y].textContent = ""
-    for (let x = 0; x < bannerlist.length; x++){
-        if (x != 0){bannertext[y].textContent += " // "}
-        bannertext[y].textContent += bannerlist[x]
-    }
+for (let x = 0; x < bannerlist.length; x++){
+    if (x != 0){bannertext.textContent += " --- "}
+    bannertext.textContent += bannerlist[x]
 }
