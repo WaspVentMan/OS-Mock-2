cc755ca99ac0b098255355d7227f8987e3e1f448
WaspVentMan <66962696+WaspVentMan@users.noreply.github.com>
2023-11-02 10:58:11 +0000

    a significant amount of hours in

--- osmock2/pages/static/js/script.js
+++ osmock2/pages/static/js/script.js
@@ -1,9 +1,12 @@
 const PHiL = "https://waspventman.co.uk"
 
-const bannertext = document.querySelector(".bannertext")
-const bannerlist = ["Health Advice Group provides weather info and health advice!", "Hello, World!", "Wow, real boring for a test text, Jacob."]
+const bannertext = document.querySelectorAll(".bannertext")
+const bannerlist = ["Health Advice Group provides weather info and health advice!", "Hello, World!", "Wow, real boring for a test text, Jacob.", "It's looking wild outside today.", "Yeah! do... that..."]
 
-bannertext.textContent = bannerlist[Math.round(Math.random()*(bannerlist.length-1))]
-setInterval(function(){
-    bannertext.textContent = bannerlist[Math.round(Math.random()*(bannerlist.length-1))]
-}, 10000)
+for (let y = 0; y < bannertext.length; y++){
+    bannertext[y].textContent = ""
+    for (let x = 0; x < bannerlist.length; x++){
+        if (x != 0){bannertext[y].textContent += " // "}
+        bannertext[y].textContent += bannerlist[x]
+    }
+}
